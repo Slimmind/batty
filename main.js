218a7c47a7ad734f15e60368d82b0bfdb8aa0720
slimmind <slimmind@gmail.com>
2018-11-08 00:03:56 +0200

    update styles

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-const doc = document;
-const batty = doc.getElementById('batty');
-const power = doc.getElementById('power');
-const battyLevel = doc.getElementById('level');
-
-
 navigator.getBattery().then(function (battery) {
-  (function updateAllBatteryInfo() {
-    updateChargeInfo();
-    updateLevelInfo();
-  })();
 
-  battery.addEventListener('chargingchange', () => updateChargeInfo());
+  const doc = document;
+  const batty = doc.getElementById('batty');
+  const power = doc.getElementById('power');
+  const battyLevel = doc.getElementById('level');
+  let chargingCounter = 0;
+  let dischargingCounter = 0;
 
   function updateChargeInfo() {
     if (battery.charging) {
-      battery.addEventListener('chargingtimechange', () => updateChargingInfo());
       updateChargingInfo();
       battery.removeEventListener('dischargingtimechange', updateDischargingInfo);
     } else {
-      battery.addEventListener('dischargingtimechange', () => updateDischargingInfo());
       updateDischargingInfo();
       battery.removeEventListener('chargingtimechange', updateChargingInfo);
     }
   }
-  // LEVEL
-  battery.addEventListener('levelchange', () => updateLevelInfo());
+
 
   function updateLevelInfo() {
     let level = battery.level;
@@ -32,39 +24,34 @@ navigator.getBattery().then(function (battery) {
     power.style.height = `${Math.floor(level * 260)}px`;
     battyLevel.innerHTML = `${Math.floor(level * 100)}%`;
     if (level < 0.7 && level > 0.4) {
-      power.style.backgroundColor = 'gold'
+      power.style.backgroundColor = '#ffcd00'
     } else if (level < 0.4) {
       power.style.backgroundColor = 'darkred';
     }
   }
-  // // CHARGING
+
+  // CHARGING
   function updateChargingInfo() {
-    // const chargingTime = doc.getElementById('chargingTime');
-    // let time = battery.chargingTime;
     batty.classList.add('charging');
-    // setTime(time, 'charging', chargingTime);
+    countCharge();
   }
-  // // DISCHARGING
+  // DISCHARGING
   function updateDischargingInfo() {
-    // const disCharge = doc.getElementById('disCharge');
-    // let time = battery.dischargingTime;
     batty.classList.remove('charging');
-    // setTime(time, 'discharging', disCharge);
   }
-  // // SET TIME 
-  // function setTime(timeVal, title, elem) {
-  //   let data = doc.createTextNode(timeVal);
-  //   let dataWrap = doc.createElement('mark');
-  //   dataWrap.appendChild(data);
-  //   doc.querySelector('main').appendChild(dataWrap);
-  //   if(timeVal < 60) {
-  //     elem.innerHTML = `${title.toUpperCase()}: ${timeVal}s`;
-  //   } else if(timeVal >= 60 && timeVal < 3600) {
-  //     elem.innerHTML = `${title.toUpperCase()}: ${Math.floor(timeVal / 60)}m`;
-  //   } else if(timeVal > 3600) {
-  //     elem.innerHTML = `${title.toUpperCase()}: ${Math.floor(timeVal / 3600)}h and ${Math.floor(timeVal % 3600 / 60)}m`;
-  //   } else {
-  //     elem.innerHTML = 'updating info...';
-  //   }
-  // }
+  // COUNT CHARGE
+  function countCharge() {
+    chargingCounter ++;
+    renderTime(chargingCounter);
+  }
+  // COUNT DISCHARGE
+  // RENDER TIME
+  function renderTime(time) {
+    console.log("TIME: ", time);
+  }
+  setInterval(() => {
+    updateChargeInfo();
+    updateLevelInfo();
+    console.log("UpdateInfo");
+  }, 1000);
 });
